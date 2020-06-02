b3855e19c55476178ee606147afde7e6572c65f0
kamry08 <61303933+kamry08@users.noreply.github.com>
2020-06-02 14:56:51 +0900

    Send-Message.9.10.11

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -52,12 +52,15 @@ $(function(){
           contentType: false
         })
         .done(function(data){
-  
           var html = buildHTML(data);
           $('.messages').append(html);
           $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
           $('form')[0].reset();
+          $('.form__submit').prop('disabled', false);
         })
+        .fail(function() {
+          alert("メッセージ送信に失敗しました");
+      });
         
     });
 });
